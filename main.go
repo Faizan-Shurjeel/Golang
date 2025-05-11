2242286758c50f4f0f649172eadf79c4f6c5cfe2
Muhammad Faizan Shurjeel <logicalbakwas@gmail.com>
2025-05-11 20:34:39 +0500

    2nd Push

--- main.go
+++ main.go
@@ -4,8 +4,24 @@ import "github.com/gin-gonic/gin"
 
 func main() {
 	r := gin.Default()
+
+	// Serve static files
+	r.Static("/public", "./public") // Serves CSS/JS
+	r.Static("/assets", "./assets") // Serves images
+
+	// Load HTML templates
+	r.LoadHTMLGlob("views/*")
+
+	// Define routes
 	r.GET("/", func(c *gin.Context) {
-		c.String(200, "Hello, World!")
+		c.HTML(200, "home.html", nil)
 	})
+	r.GET("/products", func(c *gin.Context) {
+		c.HTML(200, "products.html", nil)
+	})
+	r.GET("/products/:id", func(c *gin.Context) {
+		c.HTML(200, "product_details.html", nil)
+	})
+
 	r.Run() // Starts on localhost:8080
 }
